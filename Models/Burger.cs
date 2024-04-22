efc51d7f41da53671a6526eef38e37929640baf3
Prisz <priscila.zuniga@udla.edu.ec>
2024-04-22 11:07:28 -0500

    Validacion del atributo Precio en la clase Burgers con Data Annotations

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -9,8 +9,27 @@ namespace PriscilaZuniga_WebCodeFirst.Models
         public string? Name { get; set; }
         public bool WithCheese { get; set; }
         [Range(0.01, 9999.99)]
+        [VerificarRango]
         public decimal Precio { get; set; }
         
         
     }
+
+    public class VerificarRango : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            decimal valor = (decimal)value;
+            if (valor < 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+
 }
